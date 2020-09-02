11765d5a5b6d72261e2a82238fdc30e6fd119b19
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2020-09-01 21:13:41 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment.prod';
+
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SocketIoModule.forRoot(config)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
